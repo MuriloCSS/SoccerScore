799e3b84702397c0511bfaafde4f72c9f1768e14
Murilo Corrêa <murilosao06@gmail.com>
2026-05-04 15:28:11 -0300

    feat: adiciona campos de entrada para os nomes dos times

--- app/src/main/java/br/edu/ifsp/scl/sc3038467/soccerscore/ui/Screens.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038467/soccerscore/ui/Screens.kt
@@ -3,6 +3,9 @@ package br.edu.ifsp.scl.sc3038467.soccerscore.ui
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -12,6 +15,10 @@ import androidx.compose.ui.unit.sp
 fun SetupScreen(
     onNavigateToSummary: (String, String, Int, Int) -> Unit
 ) {
+
+    var teamAName by rememberSaveable { mutableStateOf("") }
+    var teamBName by rememberSaveable { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -20,5 +27,19 @@ fun SetupScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Configuração da Partida", fontSize = 24.sp, modifier = Modifier.padding(bottom = 16.dp))
+
+        OutlinedTextField(
+            value = teamAName,
+            onValueChange = { teamAName = it },
+            label = { Text("Nome do Time A") },
+            modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+        )
+
+        OutlinedTextField(
+            value = teamBName,
+            onValueChange = { teamBName = it },
+            label = { Text("Nome do Time B") },
+            modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+        )
     }
 }
